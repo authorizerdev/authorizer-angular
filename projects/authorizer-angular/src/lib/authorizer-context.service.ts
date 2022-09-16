6709b2af9927e8b6adeeab30c05a195f984252a8
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-17 00:39:17 +0530

    feat: add authorizer context service

--- projects/authorizer-angular/src/lib/authorizer-context.service.ts
+++ projects/authorizer-angular/src/lib/authorizer-context.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthorizerContextService {
   constructor() {}
+
+  private state$ = new BehaviorSubject<any>('initialState');
+
+  setState(myChange: any) {
+    this.state$.next(myChange);
+  }
+
+  getState() {
+    return this.state$.asObservable();
+  }
 }
