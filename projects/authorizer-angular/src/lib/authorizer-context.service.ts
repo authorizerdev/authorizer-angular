c563ce2da63e311e80492ed65148936e49606a1c
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-17 13:06:06 +0530

    feat: add utils and authorizer js

--- projects/authorizer-angular/src/lib/authorizer-context.service.ts
+++ projects/authorizer-angular/src/lib/authorizer-context.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Authorizer } from '@authorizerdev/authorizer-js';
+import { hasWindow } from './utils/window';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +9,37 @@ import { BehaviorSubject } from 'rxjs';
 export class AuthorizerContextService {
   constructor() {}
 
-  private state$ = new BehaviorSubject<any>('initialState');
+  private state$ = new BehaviorSubject<any>({
+    config: {
+      authorizerURL: '',
+      redirectURL: '/',
+      client_id: '',
+      is_google_login_enabled: false,
+      is_github_login_enabled: false,
+      is_facebook_login_enabled: false,
+      is_linkedin_login_enabled: false,
+      is_apple_login_enabled: false,
+      is_twitter_login_enabled: false,
+      is_email_verification_enabled: false,
+      is_basic_authentication_enabled: false,
+      is_magic_link_login_enabled: false,
+      is_sign_up_enabled: false,
+      is_strong_password_enabled: true,
+    },
+    user: null,
+    token: null,
+    loading: false,
+    setLoading: () => {},
+    setToken: () => {},
+    setUser: () => {},
+    setAuthData: () => {},
+    authorizerRef: new Authorizer({
+      authorizerURL: `http://localhost:8080`,
+      redirectURL: hasWindow() ? window.location.origin : '/',
+      clientID: '',
+    }),
+    logout: async () => {},
+  });
 
   setState(myChange: any) {
     this.state$.next(myChange);
