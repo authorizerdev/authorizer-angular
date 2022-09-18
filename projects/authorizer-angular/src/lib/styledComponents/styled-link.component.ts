9e8561c243a1fb28d2a888d5dfde93a9707f0a18
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-19 02:35:13 +0530

    fix: components

--- projects/authorizer-angular/src/lib/styledComponents/styled-link.component.ts
+++ projects/authorizer-angular/src/lib/styledComponents/styled-link.component.ts
@@ -3,16 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'styled-link',
   template: `
-    <a class="styled-link" style="margin-bottom: {{ marginBottom }};">
+    <span class="styled-link" style="margin-bottom: {{ marginBottom }};">
       <ng-content></ng-content>
-    </a>
+    </span>
   `,
   styles: [
     `
       .styled-link {
         color: var(--primary-color);
         cursor: pointer;
-        text-decoration: none !important;
       }
     `,
   ],
