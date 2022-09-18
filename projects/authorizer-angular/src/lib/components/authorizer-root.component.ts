bdd2c993793b1dba77956eb1f8c40a3f4f628211
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-18 13:16:49 +0530

    fix: authorizer root

--- projects/authorizer-angular/src/lib/components/authorizer-root.component.ts
+++ projects/authorizer-angular/src/lib/components/authorizer-root.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AuthorizerContextService } from '../authorizer-context.service';
+import { Views } from '../constants';
+import { createRandomString } from '../utils/common';
+import { hasWindow } from '../utils/window';
 
 @Component({
   selector: 'authorizer',
@@ -8,7 +12,40 @@ import { Component, OnInit, Input } from '@angular/core';
 export class AuthorizerRoot implements OnInit {
   @Input() body: string = 'Authorizer Root Component';
 
-  constructor() {}
+  constructor(private contextService: AuthorizerContextService) {
+    contextService.getState().subscribe((state) => {
+      this.state = state;
+    });
+  }
 
-  ngOnInit(): void {}
+  state: Record<string, any> = {};
+  view: string = Views.Login;
+  urlProps: Record<string, any> = {};
+
+  setView(viewType: string) {
+    if (viewType) this.view = viewType;
+  }
+
+  ngOnInit(): void {
+    const searchParams = new URLSearchParams(
+      hasWindow() ? window.location.search : ``
+    );
+    const paramsState = searchParams.get('state') || createRandomString();
+    const scope = searchParams.get('scope')
+      ? searchParams.get('scope')?.toString().split(' ')
+      : ['openid', 'profile', 'email'];
+    const urlProps: any = {
+      state: paramsState,
+      scope,
+    };
+    const redirectURL =
+      searchParams.get('redirect_uri') || searchParams.get('redirectURL');
+    if (redirectURL) {
+      urlProps.redirectURL = redirectURL;
+    } else {
+      urlProps.redirectURL = hasWindow() ? window.location.origin : redirectURL;
+    }
+    urlProps.redirect_uri = urlProps.redirectURL;
+    this.urlProps = urlProps;
+  }
 }
