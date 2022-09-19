b1d7eba8c3f1a4bf6e8f4cf31162d63581f8da66
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-19 15:30:29 +0530

    feat: add message component

--- projects/authorizer-angular/src/lib/components/message.component.ts
+++ projects/authorizer-angular/src/lib/components/message.component.ts
@@ -1,12 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MessageType } from '../constants';
+import { capitalizeFirstLetter } from '../utils/common';
 
 @Component({
   selector: 'message',
-  template: `<div>Message Component</div>`,
-  styles: [],
+  template: `
+    <styled-message-wrapper [type]="type">
+      <styled-flex>
+        <div style="flex: 1;">{{ formatText(text) }}</div>
+        <ng-container *ngIf="showClose">
+          <span style="cursor: pointer;" (click)="onCloseHandler()">
+            <close></close>
+          </span>
+        </ng-container>
+      </styled-flex>
+    </styled-message-wrapper>
+  `,
 })
-export class Message implements OnInit {
+export class Message {
+  @Input() type: string = MessageType.Success;
+  @Input() text: string = '';
+  @Input() showClose: boolean = false;
+
+  @Output() onClose = new EventEmitter<string>();
+
   constructor() {}
 
-  ngOnInit(): void {}
+  formatText: Function = capitalizeFirstLetter;
+
+  onCloseHandler() {
+    if (this.type === MessageType.Error) {
+      this.onClose.emit('error');
+    } else {
+      this.onClose.emit('successMessage');
+    }
+  }
 }
