f7e5671a902522997e4d5472225fb79bf3704dbb
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-19 20:06:31 +0530

    fix: basic auth login

--- projects/authorizer-angular/src/lib/authorizer.module.ts
+++ projects/authorizer-angular/src/lib/authorizer.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import {
   AuthorizerProvider,
   AuthorizerBasicAuthLogin,
@@ -60,7 +61,7 @@ import {
     StyledFlex,
     StyledMessageWrapper,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   exports: [
     AuthorizerProvider,
     AuthorizerBasicAuthLogin,
--- projects/authorizer-angular/src/lib/components/authorizer-basic-auth-login.component.ts
+++ projects/authorizer-angular/src/lib/components/authorizer-basic-auth-login.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthorizerContextService } from '../authorizer-context.service';
-import { MessageType, Views } from '../constants';
+import { ButtonAppearance, MessageType, Views } from '../constants';
 
 @Component({
   selector: 'authorizer-basic-auth-login',
   template: `
-    <ng-container *ngIf="otpData['isScreenVisible']; else other_content">
+    <ng-container *ngIf="otpData['isScreenVisible']; else login_form">
       <authorizer-verify-otp
         [onLogin]="onLogin"
         [email]="otpData['email']"
         [changeViewEventEmitter]="changeView"
       ></authorizer-verify-otp>
     </ng-container>
-    <ng-template #other_content>
+    <ng-template #login_form>
       <div>
         <ng-container *ngIf="componentState['error']">
           <message
@@ -22,6 +23,60 @@ import { MessageType, Views } from '../constants';
             (onClose)="onCloseHandler($event)"
           ></message>
         </ng-container>
+        <form [formGroup]="loginForm">
+          <div class="styled-form-group">
+            <label for="email" class="form-input-label"
+              ><span> * </span>Email
+            </label>
+            <input
+              id="email"
+              type="text"
+              formControlName="email"
+              placeholder="eg. foo@bar.com"
+              class="form-input-field {{email?.errors?.['required'] && (email?.dirty) && 'input-error-content'}}"
+            />
+            <div
+              *ngIf="email?.errors?.['required'] && (email?.dirty)"
+              class="form-input-error"
+            >
+              Email is required.
+            </div>
+          </div>
+          <div class="styled-form-group">
+            <label for="password" class="form-input-label"
+              ><span> * </span>Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              formControlName="password"
+              placeholder="********"
+              class="form-input-field {{password?.errors?.['required'] && (password?.dirty) && 'input-error-content'}}"
+            />
+            <div
+              *ngIf="password?.errors?.['required'] && (password?.dirty)"
+              class="form-input-error"
+            >
+              Password is required.
+            </div>
+          </div>
+          <br />
+          <styled-button
+            [appearance]="buttonAppearance.Primary"
+            [disabled]="
+              componentState['loading'] ||
+              !email?.value ||
+              !password?.value ||
+              email?.errors ||
+              password?.errors
+            "
+          >
+            <ng-container *ngIf="componentState['loading']; else login_text">
+              Processing ...
+            </ng-container>
+            <ng-template #login_text> Log In </ng-template>
+          </styled-button>
+        </form>
         <styled-footer>
           <styled-link
             (click)="setView(views.ForgotPassword)"
@@ -39,7 +94,49 @@ import { MessageType, Views } from '../constants';
       </div>
     </ng-template>
   `,
-  styles: [],
+  styles: [
+    `
+      .styled-form-group {
+        width: 100%;
+        border: 0px;
+        background-color: var(--white-color);
+        padding: 0 0 15px;
+      }
+      .form-input-label {
+        padding: 2.5px;
+      }
+      .form-input-label > span {
+        color: var(--danger-color);
+      }
+      .form-input-field {
+        width: 100%;
+        margin-top: 5px;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border-radius: var(--radius-input);
+        border: 1px;
+        border-style: solid;
+        border-color: var(--text-color);
+      }
+      .input-error-content {
+        border-color: var(--danger-color) !important;
+      }
+      .input-error-content:hover {
+        outline-color: var(--danger-color);
+      }
+      .input-error-content:focus {
+        outline-color: var(--danger-color);
+      }
+      .form-input-error {
+        font-size: 12px;
+        font-weight: 400;
+        color: red;
+        border-color: var(--danger-color);
+      }
+    `,
+  ],
 })
 export class AuthorizerBasicAuthLogin implements OnInit {
   @Input() onLogin: any;
@@ -56,6 +153,7 @@ export class AuthorizerBasicAuthLogin implements OnInit {
   views: any = Views;
   state: Record<string, any> = {};
   messageType: any = MessageType;
+  buttonAppearance: any = ButtonAppearance;
   componentState: Record<string, any> = {
     loading: false,
     error: null,
@@ -64,6 +162,19 @@ export class AuthorizerBasicAuthLogin implements OnInit {
     isScreenVisible: false,
     email: null,
   };
+
+  loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
+  });
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 
   onCloseHandler(stateKey: string) {
     this.componentState[stateKey] = null;
