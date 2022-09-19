55e97ff81e0b018605578fd886857089329b2bb0
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-19 20:41:24 +0530

    chore: move input form styles to separate css file

--- projects/authorizer-angular/src/lib/components/authorizer-basic-auth-login.component.ts
+++ projects/authorizer-angular/src/lib/components/authorizer-basic-auth-login.component.ts
@@ -94,49 +94,7 @@ import { ButtonAppearance, MessageType, Views } from '../constants';
       </div>
     </ng-template>
   `,
-  styles: [
-    `
-      .styled-form-group {
-        width: 100%;
-        border: 0px;
-        background-color: var(--white-color);
-        padding: 0 0 15px;
-      }
-      .form-input-label {
-        padding: 2.5px;
-      }
-      .form-input-label > span {
-        color: var(--danger-color);
-      }
-      .form-input-field {
-        width: 100%;
-        margin-top: 5px;
-        padding: 10px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        border-radius: var(--radius-input);
-        border: 1px;
-        border-style: solid;
-        border-color: var(--text-color);
-      }
-      .input-error-content {
-        border-color: var(--danger-color) !important;
-      }
-      .input-error-content:hover {
-        outline-color: var(--danger-color);
-      }
-      .input-error-content:focus {
-        outline-color: var(--danger-color);
-      }
-      .form-input-error {
-        font-size: 12px;
-        font-weight: 400;
-        color: red;
-        border-color: var(--danger-color);
-      }
-    `,
-  ],
+  styleUrls: ['../styles/input-form.styles.css'],
 })
 export class AuthorizerBasicAuthLogin implements OnInit {
   @Input() onLogin: any;
